ab67840735556c27cc3f01885446bde814b91c93
Moshe Ben Shoham <mosheb@perfectomobile.com>
2016-03-09 15:59:34 +0200

    supporting retention policy

--- run-services.sh
+++ run-services.sh
@@ -18,27 +18,26 @@ trap shutdown_awslogs INT TERM HUP
 # initial_position = start_of_file
 # log_group_name = nginx-server
 
-LOGFILE=${AWS_LOGFILE:-"/mnt/logs/access.log"}
-LOGFORMAT=${AWS_LOGFORMAT:-"%d/%b/%Y:%H:%M:%S %z"}
+LOG_FILE=${AWS_LOGFILE:-"/mnt/logs/access.log"}
+LOG_FORMAT=${AWS_LOGFORMAT:-"%d/%b/%Y:%H:%M:%S %z"}
 DURATION=${AWS_DURATION:-"5000"}
-GROUPNAME=${AWS_GROUPNAME:-"nginx-server"}
-LOGSTREAMNAME=${AWS_LOGSTREAMNAME:-'{instance_id}'}
+GROUP_NAME=${AWS_GROUPNAME:-"nginx-server"}
+LOG_STREAM_NAME=${AWS_LOG_STREAM_NAME:-'{instance_id}'}
+LOG_GROUP_RETENTION_POLICY_DAYS=${AWS_LOG_GROUP_RETENTION_POLICY_DAYS:-'90'}
 REGION=${AWS_REGION:-"eu-west-1"}
-
-echo $LOGSTREAMNAME > /var/awslogs/etc/debug.txt
 
 cp -f /awslogs.conf.dummy /var/awslogs/etc/awslogs.conf
 
 cp -f /aws.conf.dummy /var/awslogs/etc/aws.conf
 
 cat >> /var/awslogs/etc/awslogs.conf <<EOF
-[${LOGFILE}]
-datetime_format = ${LOGFORMAT}
-file = ${LOGFILE}
+[${LOG_FILE}]
+datetime_format = ${LOG_FORMAT}
+file = ${LOG_FILE}
 buffer_duration = ${DURATION}
-log_stream_name = ${LOGSTREAMNAME}
+log_stream_name = ${LOG_STREAM_NAME}
 initial_position = start_of_file
-log_group_name = ${GROUPNAME}
+log_group_name = ${GROUP_NAME}
 
 EOF
 
@@ -48,6 +47,13 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 
 EOF
+
+# Trying to create the log group so we can set a policy on it. It will simply fail if the 
+# log group already exists
+aws logs create-log-group $GROUP
+
+# Set retention policy
+aws logs put-retention-policy --log-group-name $GROUP --retention-in-days ${LOG_GROUP_RETENTION_POLICY_DAYS}
 
 /var/awslogs/bin/awslogs-agent-launcher.sh &
 
